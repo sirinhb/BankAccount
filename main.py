b3f3364e1946e97806303b3aacaae548f5ec613c
Avery Wood <awood73@charlotte.edu>
2025-02-12 17:27:54 -0500

    Adding method implementation for avery

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from SavingsAccount import SavingsAccount
 from CheckingAccount import CheckingAccount
 
+# Savings Account Instances
 averySavings = SavingsAccount("Avery Wood", 1500, 1.5)
 
-
+# Checking Account instances
 averyChecking = CheckingAccount("Avery Wood", 1600, 1000, 500)
+
+
+# Avery Instances
+averySavings.applyInterest()
+averyChecking.transfer(550)
+averyChecking.transfer(300)
+averySavings.applyInterest()
