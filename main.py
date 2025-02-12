f83d2fd114ef34fdaf5b2596dd6a74ebc2c536de
Avery Wood <awood73@charlotte.edu>
2025-02-12 17:18:06 -0500

    Add Main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import SavingsAccount
-import CheckingAccount
+from SavingsAccount import SavingsAccount
+from CheckingAccount import CheckingAccount
 
+averySavings = SavingsAccount("Avery Wood", 1500, 1.5)
+
+
+averyChecking = CheckingAccount("Avery Wood", 1600, 1000, 500)
