594e249bb7c29483d7a505c963a1dcaa12329e69
Sirin <sblan@charlotte.edu>
2025-02-12 17:50:03 -0500

    resolved merge conflicts

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-class SavingsAccount:
-    def __init__(self, savingsAccountBalance, interest):
-=======
 from BankAccount import BankAccount
 class SavingsAccount:
     def __init__(self, name, savingsAccountBalance, interest):
         self.name = name
->>>>>>> 8dbc698baed033eaf940ad8ec46ce9b3687d23cb
         self.savingsAccountBalance = savingsAccountBalance
         self.interest = interest
 
     def applyInterest(self):
-<<<<<<< HEAD
-        savingsAccountBalance = self.savingsAccountBalance * self.interest
-        print(f'Savings Account Balance: {savingsAccountBalance} Amount of Interest: {savingsAccountBalance * self.interest}')
-        return savingsAccountBalance
-=======
         self.savingsAccountBalance = self.savingsAccountBalance * (self.interest / 100)
         print(f'Savings Account Balance: {self.savingsAccountBalance} Amount of Interest: {self.savingsAccountBalance * self.interest}')
         return self.savingsAccountBalance
->>>>>>> 8dbc698baed033eaf940ad8ec46ce9b3687d23cb
+
