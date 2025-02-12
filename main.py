6494b544f380b92f4354c0b40e18ec2068a47b43
Sirin <sblan@charlotte.edu>
2025-02-12 17:25:30 -0500

    Added instances for savings and checking account classes

---
+++ main.py
@@ -0,0 +1,9 @@
+from SavingsAccount import SavingsAccount
+from CheckingAccount import CheckingAccount
+
+averySavings = SavingsAccount("Avery Wood", 1500, 1.5)
+sirinSavings= SavingsAccount("Sirin Blan", 5000, 2.0)
+
+
+averyChecking = CheckingAccount("Avery Wood", 1600, 1000, 500)
+sirinChecking = CheckingAccount("Sirin Blan", 2000, 500, 1000)
