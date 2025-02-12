b7ebdaf5b0fb5ca379bfe34c1047a5397f26cf65
Avery Wood <awood73@charlotte.edu>
2025-02-12 18:00:54 -0500

    Finishing

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from CheckingAccount import CheckingAccount
 
 # Savings Account Instances
 averySavings = SavingsAccount("Avery Wood", 1500, 1.5)
+sirinSavings = SavingsAccount("Sirin Blan", 5000, 2.0)
 
 # Checking Account instances
 averyChecking = CheckingAccount("Avery Wood", 1600, 1000, 1656, 6453, 500)
-
+sirinChecking = CheckingAccount("Sirin Blan", 2000, 500,1234, 5678, 1000)
 
 # Avery Instances
 averySavings.applyInterest()
 averyChecking.transfer(550)
 averyChecking.transfer(300)
 averySavings.applyInterest()
+
+# Sirin Instances
+sirinSavings.applyInterest()
+sirinChecking.transfer(1000)
+sirinChecking.transfer(500)
+sirinSavings.applyInterest()
