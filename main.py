2f8f9a5bde8f34295658d68707ed03e33c4d93e2
Sirin <sblan@charlotte.edu>
2025-02-12 17:29:27 -0500

    called applyInterest and transfer methods

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from CheckingAccount import CheckingAccount
 
 averySavings = SavingsAccount("Avery Wood", 1500, 1.5)
 sirinSavings= SavingsAccount("Sirin Blan", 5000, 2.0)
-
+sirinSavings.applyInterest()
 
 averyChecking = CheckingAccount("Avery Wood", 1600, 1000, 500)
 sirinChecking = CheckingAccount("Sirin Blan", 2000, 500, 1000)
+sirinChecking.transfer()
